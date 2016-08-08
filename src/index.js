28ca3bd8917327a7194ad1712c63010dc765dffd
William Castandet <wcastand@gmail.com>
2016-08-08 15:26:19 +0200

    refactor(all): add main functionnlity

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-const default_options = { }
+import request from 'request'
 
-export default function(opts) {
-  const options = Object.assign({}, default_options, opts)
+export default function(username) {
+  const options = {
+    method: 'GET',
+    url : 'https://medium.com/@' + username + '/latest',
+    headers: {
+      accept: 'application/json'
+    }
+  }
 
   return (files, metalsmith, done) => {
-    Object.keys(files)
-    .forEach(file => {
-
+    const metadata = metalsmith.metadata()
+    request(options, (error, response, body) => {
+      if (error) throw new Error(error)
+      const res = JSON.parse(body.replace('])}while(1);</x>', ''))
+      const posts = res.payload.references.Post
+      const user_infos = res.payload.user
+      metadata.medium = {
+        user_infos
+      , posts
+      }
+      done()
     })
-    done()
   }
 }
